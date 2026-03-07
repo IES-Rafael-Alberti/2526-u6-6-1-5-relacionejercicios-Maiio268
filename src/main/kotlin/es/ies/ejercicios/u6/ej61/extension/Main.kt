ce3f736a715270cf776edd6a8ed7c9213aa9b03a
Maiio268 <mmonber326@g.educaand.es>
2026-03-07 17:29:11 +0100

    commit

--- src/main/kotlin/es/ies/ejercicios/u6/ej61/extension/Main.kt
+++ src/main/kotlin/es/ies/ejercicios/u6/ej61/extension/Main.kt
@@ -1,10 +1,10 @@
 package es.ies.ejercicios.u6.ej61.extension
 
 fun main(){
-    // Asignamos a una variable de la superclase una instancia de cada subclase
-    var persona1: Persona = Estudiante("Marcos", 1, 2)
-    var persona2: Persona = Obrero("Rafael", 2)
+    var estudiante = Estudiante("Jacobo", 1, 2)
+    var obrero = Obrero("Adrian", 2)
 
-    persona1.
+    estudiante.estudiar()
+    obrero.construir()
 
 }
